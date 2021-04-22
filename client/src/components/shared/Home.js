d50b7ce5702888e59a08b23c9850494e353b3922
Harry Dhillon <navbirsdhillon@gmail.com>
2021-04-21 18:51:01 -0600

    animation created

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,7 +1,13 @@
+import { Image } from 'semantic-ui-react';
+import { MainHead, SecHead, MainImg } from '../styledComponents/sharedStyles';
 const Home = () => (
   <>
-    <h1>Home page</h1>
+    <MainHead>Home page</MainHead>
+    <SecHead>Home page as well</SecHead>
+    <MainImg 
+      src="https://images.unsplash.com/photo-1612392166886-ee8475b03af2?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2251&q=80"
+      alt="hot dogs" 
+    />
   </>
 )
-
 export default Home;
